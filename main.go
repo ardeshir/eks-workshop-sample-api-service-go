47698b7d918d91637b31ddac0c83ce0636d14c2f
sepahsalar <ardeshir.org@gmail.com>
2019-10-09 10:05:08 -0500

    message updated
    fsdi-nonprod

--- main.go
+++ main.go
@@ -33,7 +33,7 @@ func main() {
 
 		io.WriteString(w, string(out))
 
-		fmt.Println("Hello world - the log message")
+		fmt.Println("fsdi-nonprod - kubernetes cluster log message")
 	})
 	http.ListenAndServe(":8080", nil)
 }
